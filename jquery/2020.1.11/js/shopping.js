05f6e21959fabe68b1c0a8d28955f0d6e14995a4
gouzi-back <77040539+gouzi-back@users.noreply.github.com>
2021-01-11 15:00:36 +0800

    js学习

---
+++ jquery/2020.1.11/js/shopping.js
@@ -0,0 +1,59 @@
+/**
+ * Created by zongjuan.wang on 2016/6/1.
+ */
+
+/*关闭窗口*/
+function close_plan(){
+    window.close();
+}
+function collection(){
+    var flag=confirm("移入收藏后，将不再购物车显示，是否继续操作?");
+    if(flag==true){
+        alert("移入收藏成功!");
+    }
+}
+function del(){
+    var flag=confirm("您确定要删除商品吗?");
+    if(flag==true){
+        alert("删除成功!");
+    }
+}
+function minus(num){
+    var prices=parseFloat(document.getElementsByName("price")[num].value).toFixed(2);
+    var count=parseInt(document.getElementsByName("amount")[num].value)-1;
+    if(count<1){
+        alert("不能再减了，再减就没有啦！");
+    }
+    else{
+        document.getElementsByName("amount")[num].value=count;
+        var totals=parseFloat(prices*count);
+        document.getElementById("price"+num).innerHTML="¥" +totals;
+        total();
+    }
+}
+function plus(num){
+    var prices=parseFloat(document.getElementsByName("price")[num].value).toFixed(2);
+    var count=parseInt(document.getElementsByName("amount")[num].value)+1;
+    document.getElementsByName("amount")[num].value=count;
+    var totals=parseFloat(prices*count);
+    document.getElementById("price"+num).innerHTML="¥" +totals;
+    total();
+}
+function total(){
+    var prices=document.getElementsByName("price");
+    var count=document.getElementsByName("amount");
+    var sum=0;
+    for(var i=0; i<prices.length;i++){
+       sum+=prices[i].value*count[i].value;
+    }
+    document.getElementById("totalPrice").innerHTML="¥" +sum;
+
+    var noot=document.getElementById("cartList");
+    //alert(noot.lastElementChild.innerHTML);
+   // alert(noot.lastElementChild.firstElementChild.innerHTML);
+    //alert(noot.lastElementChild.firstElementChild.nextElementSibling.innerHTML);
+    //alert(noot.lastElementChild.firstElementChild.nextElementSibling.nextElementSibling.innerHTML);
+    var zongjia=noot.lastElementChild.firstElementChild.innerHTML;
+    document.getElementById("sw").innerHTML=zongjia;
+}
+total();
